aa16132c26dc2eaafe4ac772542ec5fcbf960b3a
joeyskeys <joey.skeys@gmail.com>
2026-05-19 21:34:04 +0800

    move guiding line drawing related stuff into a backup file

--- plugins/py/vam_tool.py
+++ plugins/py/vam_tool.py
@@ -30,52 +30,6 @@ def maya_useNewAPI():
     pass
 
 
-class VamGuidingLineOp(omr.MUserRenderOperation):
-    def __init__(self, name, vc):
-        super(VamGuidingLineOp, self).__init__(name)
-        self.vam_context = vc
-
-    def execute(self, draw_ctx):
-        return True
-
-    def hasUIDrawables(self):
-        return True
-
-    def addUIDrawables(self, draw_mgr, frame_ctx):
-        vc = self.vam_context
-        if vc is None:
-            return
-
-        state = vc.vam_core.get_current_state()
-        axis = getattr(vc.vam_core, 'axis', 'none')
-        if state not in ('translate', 'rotate', 'scale') or axis not in ('x', 'y', 'z'):
-            return
-
-        lines = vc.vam_core.get_axis_guide_lines()
-        if not lines:
-            return
-
-        color = {
-            'x': om.MColor((1.0, 0.1, 0.1, 0.85)),
-            'y': om.MColor((0.2, 1.0, 0.2, 0.85)),
-            'z': om.MColor((0.25, 0.45, 1.0, 0.85)),
-        }.get(axis, om.MColor((1.0, 1.0, 0.0, 0.85)))
-
-        draw_mgr.beginDrawable()
-        try:
-            try:
-                draw_mgr.setColor(color)
-                draw_mgr.setLineWidth(2.0)
-                draw_mgr.setLineStyle(omr.MUIDrawManager.kSolid)
-            except Exception:
-                pass
-
-            for p1, p2 in lines:
-                draw_mgr.line(p1, p2)
-        finally:
-            draw_mgr.endDrawable()
-
-
 class VamRenderOverride(omr.MRenderOverride):
     def __init__(self, name, vc):
         super(VamRenderOverride, self).__init__(name)
@@ -168,8 +122,6 @@ class VamContext(omui.MPxContext):
         else:
             self._vam_prev_hotkey_set = None
 
-        self._register_render_override()
-
     def toolOffCleanup(self):
         """Called when tool is deactivated."""
         print("VAM Tool Deactivated")
@@ -179,32 +131,6 @@ class VamContext(omui.MPxContext):
         if HOTKEY_CONTEXT_AVAILABLE:
             deactivate_vam_hotkey_context()
             restore_vam_tool_hotkey_set(getattr(self, "_vam_prev_hotkey_set", None))
-
-    def _register_render_override(self):
-        print('in register render override')
-        self._deregister_render_override()
-        renderer = omr.MRenderer.theRenderer()
-        if renderer is None:
-            return
-        override_name = f'vamRenderOverride_{id(self)}'
-        override = VamRenderOverride(override_name, self)
-        try:
-            renderer.registerOverride(override)
-            self._render_override = override
-        except Exception:
-            self._render_override = None
-
-    def _deregister_render_override(self):
-        override = self._render_override
-        if override is None:
-            return
-        renderer = omr.MRenderer.theRenderer()
-        if renderer is not None:
-            try:
-                renderer.deregisterOverride(override)
-            except Exception:
-                pass
-        self._render_override = None
 
     def doPress(self, event, drawMgr, frameContext):
         """
